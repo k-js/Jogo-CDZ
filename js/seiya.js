a9643e20771a4509312b9abe7edac6f232614edd
Keu Santos <modulodigital69@gmai.com>
2022-03-25 03:21:04 -0300

    javascript

---
+++ js/seiya.js
@@ -0,0 +1,22 @@
+
+//Persanagem Saiya 
+function escolhaSaiygaFase(num,respostaCerta) {
+    var pagina = num + 1;
+    var tentativas = 0;
+    var resposta = window.prompt("Selecione a resposta!");
+
+    while(resposta != respostaCerta){
+        if (tentativas <= 1){
+            tentativas += 1;
+            alert(" Resposta errada. tentativa " + tentativas.toString() )
+            resposta = window.prompt("Selecione uma resposta")
+        } else {
+            alert("Lamentamos vc perdeu o Jogo! ")
+            window.location.href = "/img/gameOver.jpg";
+        }
+    }
+    if (pagina == 4){
+        return window.location.href = "/Personagens_CDZ/Seiya/fimDeJogo.html";
+    }
+    return  window.location.href = "saiyaFase" + pagina + ".html";
+}
---
+++ js/shun.js
@@ -0,0 +1,20 @@
+function escolhaShunFase(num,respostaCerta) {
+    var pagina = num + 1;
+    var tentativas = 0;
+    var resposta = window.prompt("Selecione a resposta!");
+
+    while(resposta != respostaCerta){
+        if (tentativas <= 1){
+            tentativas += 1;
+            alert(" Resposta errada. tentativa " + tentativas.toString() )
+            resposta = window.prompt("Selecione uma resposta")
+        } else {
+            alert("Lamentamos vc perdeu o Jogo! ")
+            window.location.href = "/img/gameOver.jpg";
+        }
+    }
+    if (pagina == 4){
+        return window.location.href = "/Personagens_CDZ/Shun/fimDeJogo.html";
+    }
+    return  window.location.href = "shunFase" + pagina + ".html";
+}
