a3943fc3efe289bcd5092a7c74e717eaaae25534
Keu Santos <modulodigital69@gmai.com>
2022-03-25 03:22:27 -0300

    pequenas alteracoes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-function inicia(){
+function inicia() {
 
-    let personagem = window.prompt("Escolha seu personagem!")
+        var personagem = window.prompt("Digite o número do personagem!")
 
-        if(personagem == "1") {
+        if (personagem == "1") {
 
                 window.location.href = "../Personagens_CDZ/Hyoga/hyogaFase1.html"
 
-        }else if(personagem == "2") {
+        } else if (personagem == "2") {
 
                 window.location.href = "../Personagens_CDZ/Seiya/saiyaFase1.html"
 
-        }else if(personagem == "3") {
+        } else if (personagem == "3") {
 
                 window.location.href = "../Personagens_CDZ/Shun/shunFase1.html"
 
-        }else {
+        } else {
                 alert("Insira o numero respectivo do personagem corretamente!")
                 inicia();
-            }
+        }
 }
+
+
+
+
+
+
