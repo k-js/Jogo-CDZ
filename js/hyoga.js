65cb8a04cd099a97a44c7866cde1eadf2005181c
Keu Santos <modulodigital69@gmai.com>
2022-03-25 11:45:26 -0300

    alterando tentativas

--- js/hyoga.js
+++ js/hyoga.js
@@ -5,7 +5,7 @@ function escolhaHyogaFase(num,respostaCerta) {
     var resposta = window.prompt("Selecione a resposta!");
 
     while(resposta != respostaCerta){
-        if (tentativas <= 1){
+        if (tentativas <= 0){
             tentativas += 1;
             alert(" Resposta errada. tentativa " + tentativas.toString() );
             resposta = window.prompt("Selecione uma resposta")
